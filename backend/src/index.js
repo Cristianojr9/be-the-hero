2f778c9ac74d725b3c59a6879651d03e37cc8bd0
Cristiano <cristianojr@alu.ufc.br>
2020-03-25 00:14:57 -0300

    backend-complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const expresss = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = expresss();
 
+app.use(cors());
 app.use(expresss.json());
 app.use(routes);
 
